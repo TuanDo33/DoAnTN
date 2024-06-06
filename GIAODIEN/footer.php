53db4ee4bbc65955d57b22bc98ef5e124b839d5c
TuanDo33Do33 <tuandoplt@gmail.com>
2024-06-06 23:17:30 +0700

    update

---
+++ GIAODIEN/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+            <div class="row gx-0" style="width:1200px;">
+                <div class="col" style="margin-top:1%;">
+                    <div style="margin-left:15%;">
+                        <div>
+                            <a href="" class="navbar-brand text-white">
+                                <img src="/Img/_3e0757bb-eae4-4033-bb53-4e6b11d61967.jpg" alt=""
+                                    style="width:65px; height:65px; margin-top: 2%;"></a>
+                        </div>
+                        <img src="/Img/logo-bct.png" alt="" style="width:150px; height:55px;margin-top:15%;">
+                    </div>
+                </div>
+                <div class="col" style="margin-top:2%;">
+                    <div class="thongtin">
+                        <p class="tieudefooter">Thông tin chung </p>
+                        <p><a href="">giới thiệu về Tstore</a></p>
+                        <p><a href="">tintức</a></p>
+                        <p><a href="">hợp tác</a></p>
+                    </div>
+                </div>
+                <div class="col" style="margin-top:2%;">
+                    <div class="thongtin">
+                        <p class="tieudefooter">Chính sách</p>
+                        <p><a href="">bảo hành</a></p>
+                        <p><a href="">mua hàng</a></p>
+                        <p><a href="">giao hàng</a></p>
+                        <p><a href="">bảo mật thông tin</a></p>
+                    </div>
+                </div>
+                <div class="col" style="margin-top:2%;">
+                    <div class="thongtin">
+                        <p class="tieudefooter">Liên hệ & Địa chỉ</p>
+                        <p class="lienhe">Hotline:0376017025</p>
+                        <p class="lienhe">Email:Tstore@gmail.com</p>
+                        <p>Địa chỉ:446 phù lưu tế mỹ đức hà nội</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
--- GIAODIEN/header.php
+++ GIAODIEN/header.php
@@ -1,34 +1,54 @@
-<header class="header">
-        <div>
-            <div class="box2">
-                <div class="container-1">
-                    <span class="icon"><i class="fa fa-search"></i></span>
-                    <input type="search" id="search" placeholder="Search..." />
+<header class="header navbar navbar-expand">
+            <a href="" class="navbar-brand text-white" style="margin-left:5%;">
+                <img src="/Img/_3e0757bb-eae4-4033-bb53-4e6b11d61967.jpg" alt="" style="width:65px; height:65px;"></a>
+            <div class="navbar-nav" style="margin-left:10%;">
+                <div class="box2" style="margin-bottom:30px;">
+                    <div class="container-1">
+                        <span class="icon"><i class="fa fa-search"></i></span>
+                        <input type="search" id="search" placeholder="Search..." />
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-    <nav class="nav">
-        <div>
-            <ul>
-
-                <li><a href="home.html">Trang chủ</a></li>       
-                <li><a href="#home">TiVi</a></li>       
-                <li><a href="#home">Tủ Lạnh</a></li>        
-                <li><a href="#home">Máy Giặt</a></li>       
-                <li><a href="#home">Điều Hòa</a></li>       
-                <li><a href="#home">Gia Dụng</a></li>        
-                <li><a href="#home">Điện Thoại</a></li>       
-                <!-- <li><a href="#news">Chia sẻ</a></li> -->
-                <li class="dropdown">
-                <a class="dropbtn">Học tập</a>
-                <!-- <div class="dropdown-content">
-                <a href="#">HTML</a>
-                <a href="#">CSS</a>
-                <a href="#">Javascript</a>
-                </div> -->
-                </li>
-                
+            <button type="button" class="btn  btn-lg navbar-nav" data-toggle="modal" data-target="#modelId"
+                style="border: none;font-size: 16px; margin-left:25%; border:2px rgb(253, 253, 253) solid;margin-top:10px;">
+                <!-- <img src="/Img/User.png" style="width:22px;height:22px;margin-left:10px;" alt=""> -->
+                <a href="#"
+                    style="text-decoration: none;font-size:16px;font-weight: bold; color: rgb(10, 10, 10); margin-left:7px;">
+                    <img src="/Img/User.png" style="width:22px;height:22px;margin-left:10px;" alt="">
+                    Đăng nhập</a>
+            </button>
+        </header>
+        <!-- nav -->
+        <nav class="nav n">
+            <div class="navbar-nav navbar-expand-lg" style="margin-left: 14%;">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="#home">Trang chủ</a></li>
+                    <li class="nav-item"><a href="#home">TiVi</a></li>
+                    <li class="nav-item"><a href="#home">Tủ Lạnh</a></li>
+                    <li class="nav-item"><a href="#home">Máy Giặt</a></li>
+                    <li class="nav-item"><a href="#home">Điều Hòa</a></li>
+                    <li class="nav-item"><a href="#home">Gia Dụng</a></li>
+                    <li class="nav-item"><a href="#home">Điện Thoại</a></li>
+                    <!-- <li><a href="#news">Chia sẻ</a></li>  -->
+                    <!-- <li class="nav-item dropdown">
+                        <a class="dropbtn ">Học tập</a>
+                        <ul class="dropdown-content">~
+                            <a class="dropdown-item" href="#">HTML</a>
+                            <a class="dropdown-item" href="#">CSS</a>
+                            <a class="dropdown-item" href="#">Javascript</a>
+                        </ul>
+                    </li> -->
+                    <li class="dropdown">
+                        <a class="dropbtn dropdown-toggle">Danh sách sản phẩm</a>
+                        <div class="dropdown-content">
+                            <a href="#">TiVi</a>
+                            <a href="#">Tủ lạnh</a>
+                            <a href="#">Máy giặt</a>
+                            <a href="#">Điều hòa</a>
+                            <a href="#">Gia dụng</a>
+                            <a href="#">Đồ cũ</a>
+                        </div>
+                    </li>
                 </ul>
-        </div>
-    </nav>
+            </div>
+        </nav>
